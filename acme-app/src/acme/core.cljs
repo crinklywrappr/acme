02154682f722e303f7a3d388a3c25fecef67db81
Daniel Fitzpatrick <crinklywrappr@pm.me>
2020-11-12 23:02:47 -0600

    fixed type hinting & release mode works now

--- acme-app/src/acme/core.cljs
+++ acme-app/src/acme/core.cljs
@@ -1,46 +1,48 @@
 (ns acme.core
   (:require [x11]))
 
-(defn handler [^x11.XClient x wid disp]
-  (let [gc (.AllocID x)
-        _ (.CreateGC x gc wid)
-        white (-> disp .-screen first .-white_pixel)
-        black (-> disp .-screen first .-black_pixel)
-        cid-white (.AllocID x)
-        cid-black (.AllocID x)
-        _ (.CreateGC x cid-white wid (clj->js {:foreground white :background black}))
-        _ (.CreateGC x cid-black wid (clj->js {:foreground black :background white}))]
+(defn handler [^js client wid disp]
+  (let [gc (.AllocID client)
+        _ (.CreateGC client gc wid)
+        ;; .-white_pixel & .-black_pixel syntactic sugar don't
+        ;; work with advanced compilation for some reason
+        white (-> disp .-screen first js->clj (get "white_pixel"))
+        black (-> disp .-screen first js->clj (get "black_pixel"))
+        cid-white (.AllocID client)
+        cid-black (.AllocID client)
+        _ (.CreateGC client cid-white wid (clj->js {:foreground white :background black}))
+        _ (.CreateGC client cid-black wid (clj->js {:foreground black :background white}))]
     (fn [ev]
       (when (== (.-type ev) 12)
         (.PolyFillRectangle
-         x wid cid-black
+         client wid cid-black
          (clj->js [0 0 500 500]))
         (.PolyText8
-         x wid cid-white
+         client wid cid-white
          50 50
          (clj->js ["Hello, Shadow-Cljs!"]))))))
 
 (defn error-handler [e]
   (println e))
 
-(defn add-handlers! [^x11.XClient x wid disp]
-  (.on x "event" (handler x wid disp))
-  (.on x "error" error-handler))
+(defn add-handlers! [^js client wid disp]
+  (.on client "event" (handler client wid disp))
+  (.on client "error" error-handler))
 
 (defn window! [disp]
   (let [exposure (.-Exposure x11/eventMask)
         pointer-motion (.-PointerMotion x11/eventMask)
-        x (.-client disp)
+        client ^js (.-client disp)
         root (-> disp .-screen first .-root)
-        wid (.AllocID ^x11.XClient x)]
+        wid (.AllocID client)]
     (.CreateWindow
-     ^x11.XClient x
+     client
      wid root
      0 0 500 500
      0 0 0 0
      (clj->js {:eventMask (bit-or exposure pointer-motion)}))
-    (.MapWindow ^x11.XClient x wid)
-    (add-handlers! x wid disp)))
+    (.MapWindow client wid)
+    (add-handlers! client wid disp)))
 
 (defn client! []
   (x11/createClient
